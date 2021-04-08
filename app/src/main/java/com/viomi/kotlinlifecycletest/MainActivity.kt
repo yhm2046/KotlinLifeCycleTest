d9f0a45f9a71b08f19b1179c82d081e66ec2218c
yhm2046 <yhm2046@gmail.com>
2021-04-08 10:18:37 +0800

    更新代码依然无法获取savedInstanceState的值

--- app/src/main/java/com/viomi/kotlinlifecycletest/MainActivity.kt
+++ app/src/main/java/com/viomi/kotlinlifecycletest/MainActivity.kt
@@ -8,10 +8,29 @@ import com.viomi.kotlinlifecycletest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val tag="MainActivity"
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(tag,"onSaveInstanceState()...")
+        super.onSaveInstanceState(outState) //临时保存数据
+        val temdata="something str"
+        outState.putString("data1",temdata)
+        val temdata2=99.99
+        outState.putDouble("data2",temdata2)
+        val temdata3=false
+        outState.putBoolean("data3",temdata3)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(tag,"onCreate..")
         val binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        if (savedInstanceState!=null){
+            Log.d(tag,"running...........")
+            Log.d(tag,"str:${savedInstanceState.getString("data1")}")
+            Log.d(tag,"double:${savedInstanceState.getDouble("data2")}")
+            Log.d(tag,"boolean:${savedInstanceState.getBoolean("data3")}")
+        }else
+            Log.d(tag,"savedInstanceState is null...........")
         binding.btnShowNormal.setOnClickListener {
             val intent = Intent(this,NormalActivity::class.java)
             startActivity(intent)
@@ -21,6 +40,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }//end oncreate
+
 
     override fun onStart() {
         super.onStart()
